f7b79f48d0767a940180bc0b44af14a15a1e5ff5
Kun-Da-Chung <c954303005@gmail.com>
2025-07-27 21:25:44 +0800

    Update zowe_operations.sh

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -1,24 +1,21 @@
- #!/bin/bash
- # zowe_operations.sh
- # Set up environment
- # cd ../IBMZ/github/check-cobol-automation
- echo "change to github dir"
- #zowe --version > $ZOWE_VERSION
- echo $ZOWE_VERSION
+#!/bin/bash
+
+# zowe_operations.sh
+
 # Convert username to lowercase
- echo "zowe operation start..."
- LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
- # Check if directory exists, create if it doesn't
- echo " uss files zid=" $LOWERCASE_USERNAME
- if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &> /dev/null; then
- echo "Directory does not exist. Creating it..."
- zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck"
- else
- echo "Directory already exists." 
- fi
- # Upload files
- echo "upload files..."
- zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.18.zip"
- # Verify upload
- echo "Verifying upload:"
- zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
+
+# Check if directory exists, create if it doesn't
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
+  echo "Directory does not exist. Creating it..."
+  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
+else
+  echo "Directory already exists."
+fi
+
+# Upload files
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.18.jar"
+
+# Verify upload
+echo "Verifying upload:"
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"

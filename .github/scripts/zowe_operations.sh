eb8cd6e21787445b360166a6e88480218f87ef9a
Kun-Da-Chung <c954303005@gmail.com>
2025-07-27 08:35:52 +0800

    Update zowe_operations.sh

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -3,19 +3,19 @@
  # Set up environment
  # cd ../IBMZ/github/check-cobol-automation
  echo "change to github dir"
- zowe --version > $ZOWE_VERSION
+ #zowe --version > $ZOWE_VERSION
  echo $ZOWE_VERSION
 # Convert username to lowercase
  echo "zowe operation start..."
  LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
  # Check if directory exists, create if it doesn't
  echo " uss files zid=" $LOWERCASE_USERNAME
- if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &> /dev/null; then
+ #if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &> /dev/null; then
  echo "Directory does not exist. Creating it..."
  zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck"
- else
+ #else
  echo "Directory already exists." 
- fi
+ #fi
  # Upload files
  echo "upload files..."
  zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive--binary-files "cobol-check-0.2.18.zip"
